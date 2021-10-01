e76618369dbabc78d73973a837c18c0357b5bdc9
henrytran721 <henryhtran07@gmail.com>
2021-10-01 13:58:17 -0700

    sorting

---
+++ sorting/mergeSort.js
@@ -0,0 +1,70 @@
+// merge sort 
+
+/**
+ *  [44, 3, 9, 81, 15, 6, 69]
+ *  
+ * [44, 3, 9, 81]  [15, 6, 69]
+ * 
+ * [44, 3] [9, 81]  [15, 6] [69]
+ *         
+ * [44] [3] [9] [81] [15] [6] [69]        
+ * // sort 
+ * [3,44] [9, 81]   [6, 15] [69]
+ * 
+ * 
+ * 
+ * [3, 9, 44, 81]   [6, 15, 69]
+ * 
+ * [3, 6, 9, 44, 69, 81]
+ * 
+ * Merge sort breaks our array down to the base case of arr.length === 1 
+ * and then builds it back up in sorted formation
+ * 
+ * Recursion 
+ */
+
+// function that sorts our arrays
+// our arrays have to be in sorted order
+
+function merge(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while(i < arr1.length && j < arr2.length) {
+        // do our comparison
+        if(arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while(i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while(j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    // sorts the left
+    let left = mergeSort(arr.slice(0, mid));
+    // sorts from mid to the end (right)
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+console.log(mergeSort([1,44,36,9,88,10,66,64,78,90]));
---
+++ sorting/quickSort.js
@@ -0,0 +1,38 @@
+/**
+ * Select a pivot point and count how many values are less than current pivot
+ * [4, 42, 10, 1, 3, 5, 30]
+ * [1, 3, 4, 42, 10, 5, 30]
+ */
+
+
+function pivot(arr, start = 0, end = arr.length) {
+    let pivot = start;
+    let pivotIndex = start;
+    
+    for(let i = start + 1; i < arr.length; i++) {
+        if(arr[i] < arr[pivot]) {
+            pivotIndex++;
+            let temp = arr[i];
+            arr[i] = arr[pivotIndex];
+            arr[pivotIndex] = temp;
+        }
+    }
+
+    let temp = arr[pivot];
+    arr[pivot] = arr[pivotIndex];
+    arr[pivotIndex] = temp;
+
+    return pivotIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if(left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+
+    return arr;
+}
+
+console.log(quickSort([4, 42, 10, 1, 3, 5, 30]));
